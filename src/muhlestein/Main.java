ce1f42e1ae3c46bb47b8d893cf4587f40dc93ded
Mark Tobler <marktobler@gmail.com>
2019-05-17 19:47:43 -0700

    I added a response. I wonder if the mac environment will make any difference, going back and forth between the two. There's probably a config thing to take care of this. WHen I opened the file on my side, everything was on one line. I easily fixed itself once told to format.

--- src/muhlestein/Main.java
+++ src/muhlestein/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello, my name is Jake.");
-        System.out.println("Hello, Jake! My name is ^-^.");
+        System.out.println("Hello, Jake! My name is ^-^. Nice to meet you!");
     }
 
 
